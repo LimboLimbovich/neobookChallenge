4c88751a401e143e2538ee0740f522c343c7e226
limbo <benz16082005@gmail.com>
2023-12-20 22:26:54 +0600

    fixed styles

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from '../assets/styles/auth.module.css'
-import { Link, NavLink } from 'react-router-dom';
 import routes from '../utils/consts';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import bg from '../assets/images/image 1.png'
+import eye from '../assets/images/eye.png'
 
 const Auth = () => {
   return (
-    <div className={styles.box}>
+    <div className={styles.container}>
+    <img src={bg} alt="" />
+    <div className={styles.box}>  
       <form>
+        
         <div className={styles.inputBox}>
           <input type="text" required="required"/>
           <span>Имя пользователя</span>
@@ -15,17 +20,18 @@ const Auth = () => {
         <div className={styles.inputBox}>
           <input type="password" required="required"/>
           <span>Пароль</span>
-      
         </div>
         <div className={styles.links}>
           <a href='/'>Забыл пароль</a>
+          <div className={styles.eyeBtn}><img src={eye} alt="" /></div>
         </div>
           <button type='login' className={styles.primaryBtn}>Войти</button>
-          <p className={styles.top}>
-            <a href={routes.REGISTRATION} className={styles.register}>Зарегистрироваться</a>
-        </p>        
-          
       </form>
+        <div className={styles.register}>
+          <a href={routes.REGISTRATION}>Зарегистрироваться</a>
+        </div>
+        
+    </div>
     </div>
   )
 }
