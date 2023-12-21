1279f58b0e883d1362f02eea2eefa22684bce612
limbo <benz16082005@gmail.com>
2023-12-21 23:50:39 +0600

    completed styles

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,39 +1,76 @@
-import React from 'react';
-import styles from '../assets/styles/auth.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from '../assets/styles/auth.module.css';
 import routes from '../utils/consts';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import bg from '../assets/images/image 1.png'
-import eye from '../assets/images/eye.png'
+import shopCart from '../assets/images/ShopCart.png'
+import bg from '../assets/images/image 1.png';
+import eye from '../assets/images/eye-disable.png';
+import lightShadow from '../assets/images/Rectangle 1.png'
+import eyeDisable from '../assets/images/eye.png';
 
 const Auth = () => {
+  const [formValid, setFormValid] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [eyeClicked, setEyeClicked] = useState(false);
+
+  useEffect(() => {
+    // Assume you have some validation logic for name and password errors.
+    // Replace with your actual validation logic.
+    const nameError = false;
+    const passwordError = false;
+
+    if (nameError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [formValid]);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+    setEyeClicked(!eyeClicked);
+  };
+
   return (
     <div className={styles.container}>
-    <img src={bg} alt="" />
-    <div className={styles.box}>  
-      <form>
-        
-        <div className={styles.inputBox}>
-          <input type="text" required="required"/>
-          <span>Имя пользователя</span>
-        </div>
+      
+      <img src={bg} className={styles.bg}/>
+      <img src={shopCart} className={styles.cart}/>
+      <img src={lightShadow} className={styles.shadow}/>
+      
 
-        <div className={styles.inputBox}>
-          <input type="password" required="required"/>
-          <span>Пароль</span>
-        </div>
-        <div className={styles.links}>
-          <a href='/'>Забыл пароль</a>
-          <div className={styles.eyeBtn}><img src={eye} alt="" /></div>
-        </div>
-          <button type='login' className={styles.primaryBtn}>Войти</button>
-      </form>
+      <div className={styles.box}>
+        <form>
+          <div className={styles.inputBox}>
+            <input type="text" required="required" />
+            <span>Имя пользователя</span>
+          </div>
+
+          <div className={styles.inputBox}>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              required="required"
+            />
+            <span>Пароль</span>
+            <div
+              className={`${styles.eyeBtn} ${eyeClicked ? styles.disabled  : ''}`}
+              onClick={togglePasswordVisibility}
+            >
+              <img src={eyeClicked ? eyeDisable : eye} alt="" />
+            </div>
+          </div>
+          <div className={styles.links}>
+            <a href='/'>Забыл пароль</a>
+          </div>
+          <button type='login' className={styles.primaryBtn} disabled={!formValid}>
+            Войти
+          </button>
+        </form>
         <div className={styles.register}>
           <a href={routes.REGISTRATION}>Зарегистрироваться</a>
         </div>
-        
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
